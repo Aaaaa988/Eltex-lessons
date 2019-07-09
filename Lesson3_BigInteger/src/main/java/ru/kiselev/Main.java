d0412263c8486aa36ce78b02e784867495422c37
Aaaaa988 <kiselev-v.s.99@yandex.ru>
2019-07-10 00:16:38 +0700

    BigInteger

---
+++ Lesson3_BigInteger/src/main/java/ru/kiselev/Main.java
@@ -0,0 +1,11 @@
+package ru.kiselev;
+
+import java.math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger(String.valueOf(2)).pow(10000);
+        System.out.println(a.toString());
+    }
+}
