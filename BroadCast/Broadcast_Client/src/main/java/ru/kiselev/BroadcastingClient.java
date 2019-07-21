9caf9b6ac3f307e1459bfa0009ccfdcd9608c0a8
Aaaaa988 <kiselev-v.s.99@yandex.ru>
2019-07-21 19:29:14 +0700

    add BroadCast Client&Server

---
+++ BroadCast/Broadcast_Client/src/main/java/ru/kiselev/BroadcastingClient.java
@@ -0,0 +1,26 @@
+package ru.kiselev;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class BroadcastingClient {
+    private static DatagramSocket socket = null;
+
+    public static void main(String[] args) throws IOException{
+        broadcast("Hello", InetAddress.getByName("192.168.0.255"));
+    }
+
+    public static void broadcast(String broadcastMessage, InetAddress address) throws IOException {
+        socket = new DatagramSocket();
+        socket.setBroadcast(true);
+        byte[] buffer = broadcastMessage.getBytes();
+        DatagramPacket packet
+                = new DatagramPacket(buffer, buffer.length, address, 1050);
+        socket.send(packet);
+        socket.close();
+    }
+
+}
+
