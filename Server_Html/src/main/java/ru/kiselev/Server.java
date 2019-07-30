684d2375e50dd439251313b05a8a193862bff5ef
Aaaaa988 <kiselev-v.s.99@yandex.ru>
2019-07-30 14:10:51 +0000

    fix Server

--- Server_Html/src/main/java/ru/kiselev/Server.java
+++ Server_Html/src/main/java/ru/kiselev/Server.java
@@ -5,11 +5,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 public class Server {
     public static void main(String[] args) throws IOException {
@@ -30,39 +26,5 @@ public class Server {
             }
 
 
-        }
     }
-
-
-    /*InputStream inStream = client.getInputStream();
-                    OutputStream outStream = client.getOutputStream();
-                    Scanner in = new Scanner(inStream);
-                    PrintWriter out = new PrintWriter(outStream);
-
-                    //System.out.println(in.nextLine());
-                    String[] request = in.nextLine().split(" ");
-                    for (String str : request) {
-                        System.out.println(str);
-                    }
-                    String HTTP_version = request[2];
-                    String Page = request[1];
-
-                    String path = "src/main/resources" + Page;
-                    File file = new File(path);
-                    if (!file.exists() && path.contains("html")) path = "src/main/resources/404.html";
-                    if (file.exists() && !path.contains("html")) path = "src/main/resources/434.html";
-                    if (!file.isFile() && !path.contains("html")) path = "src/main/resources/444.html";
-
-                    String htmlPage = Files.readString(Paths.get(path));
-                    htmlPage = HTTP_version + " 200 OK\n" +
-                            "Сontent-Length:" + htmlPage.length() + "\n" +
-                            "Сontent-Type:text/html\n" +
-                            "\n"
-                            + htmlPage;
-
-
-                    //System.out.println(in.nextLine());
-                    out.write(htmlPage);
-                    out.flush();
-
-     */
+}
